febe999ba3e13f2fd28273464dce08d338a0bb00
shrutikkupekar <shrutikskupekar@gmail.com>
2026-03-28 23:16:57 -0700

    resume updated

--- assets/assets.js
+++ assets/assets.js
@@ -164,7 +164,7 @@ export const serviceData = [
 
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript, MERN Stack,Git' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description:  'M.S. in Computer Science (USA)' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description:  'M.S. in Computer Science (CSUF)' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
 
